0f999bdd93cb38a763758c0c1f79b2ebabefbb2b
Denis Repyev <sinedviper@gmail.com>
2022-09-21 17:22:39 +0300

    add visual and component and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App(): React.JSX {
+import Change from "./pages/Change";
+import Home from "./pages/Home";
+import styles from "./App.module.css";
+
+function App(): JSX.Element {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+    <>
+      <header className={styles.header}>
+        <h1>CRUD</h1>
       </header>
-    </div>
+      <main className={styles.main}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Navigate to='/users' />} />
+            <Route path='/users' element={<Home />} />
+            <Route path='/users/:idUser' element={<Home />} />
+            <Route path='/users/:idUser/edit' element={<Change />} />
+            <Route path='/users/create' element={<Change />} />
+          </Routes>
+        </Router>
+      </main>
+      <footer className={styles.footer}>
+        <span>Created by Denis Repyev(c)</span>
+      </footer>
+    </>
   );
 }
 
